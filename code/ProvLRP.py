09f227f7e3c0d89ef965c4370ca6831e40b07a5c
Red Rapious <red.rapious@gmail.com>
2024-06-07 09:52:13 +0200

    Removed standard for abstract semiring

--- code/ProvLRP.py
+++ code/ProvLRP.py
@@ -5,18 +5,29 @@ class MultipleInitialisation(Exception):
     pass
 
 class Semiring():
-    def handle_initialisation(self, not_none, n1, n2, n3):
+    def handle_initialisation(self, not_none, n1, n2):
         if not_none is not None:
                     assert isinstance(not_none, numbers.Number), f"must be a number, is of type {type(not_none)}"
                     self.value = not_none
-                    if n1 is not None or n2 is not None or n3 is not None:
+                    if n1 is not None or n2 is not None:
                         raise MultipleInitialisation
+                    
+    def handle_relevance(self, relevance, n1, n2):
+         # for other semiring, apply a transform to the first argument
+         self.handle_initialisation(relevance, n1, n2)
+
+    def handle_activation(self, activation, n1, n2):
+         # for other semiring, apply a transform to the first argument
+         self.handle_initialisation(activation, n1, n2)
     
-    def __init__(self, relevance=None, activation=None, weight=None, standard=None):
-        self.handle_initialisation(relevance, activation, weight, standard)
-        self.handle_initialisation(activation, relevance, weight, standard)
-        self.handle_initialisation(weight, activation, relevance, standard)
-        self.handle_initialisation(standard, activation, weight, relevance)
+    def handle_weight(self, weight, n1, n2):
+         # for other semiring, apply a transform to the first argument
+         self.handle_initialisation(weight, n1, n2)
+
+    def __init__(self, relevance=None, activation=None, weight=None):
+        self.handle_relevance(relevance, activation, weight)
+        self.handle_activation(activation, relevance, weight)
+        self.handle_weight(weight, activation, relevance)
 
     def __mul__(self, b):
         f = self.value * b.value
@@ -51,16 +62,14 @@ class Semiring():
         s.value = f
         return s
     
-    def lower_bound(A, bound):
-        return np.vectorize(identity)(bound*np.ones(A[0].shape))
+    def lower_bound(shape, bound):
+        return np.vectorize(from_activation)(bound*np.ones(shape))
 
-    def upper_bound(A, bound):
-        return np.vectorize(identity)(bound*np.ones(A[0].shape))
+    def upper_bound(shape, bound):
+        return np.vectorize(from_activation)(bound*np.ones(shape))
     
-#from_relevance = np.vectorize(lambda x : Semiring(relevance=x))
 from_activation = np.vectorize(lambda x : Semiring(activation=x))
 from_weight = np.vectorize(lambda x : Semiring(weight=x))
-identity = np.vectorize(lambda x : Semiring(standard=x))
 pos = np.vectorize(lambda x : x.pos())
 neg = np.vectorize(lambda x : x.neg())
 
