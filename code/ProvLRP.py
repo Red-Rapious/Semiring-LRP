5b815a49304d53466b6c6818c99ee6e6509d5b52
Red Rapious <red.rapious@gmail.com>
2024-06-10 15:00:11 +0200

    Added tropical semiring

--- code/ProvLRP.py
+++ code/ProvLRP.py
@@ -139,18 +139,6 @@ class BooleanSemiring():
         s.value = f
         return s
     
-    def pos(self):
-        assert False, "not defined for boolean semiring"
-
-    def neg(self):
-        assert False, "not defined for boolean semiring"
-    
-    def lower_bound(shape, _bound):
-        assert False, "not defined for boolean semiring"
-
-    def upper_bound(shape, _bound):
-        assert False, "not defined for boolean semiring"
-    
     from_activation = np.vectorize(lambda x : BooleanSemiring(activation=x))
     from_weight = np.vectorize(lambda x : BooleanSemiring(weight=x))
     vect_pos = np.vectorize(lambda x : x.pos())
@@ -164,3 +152,55 @@ class BooleanSemiring():
     
     def __repr__(self):
         return f"BooleanSemiring({self.value})"
+
+
+class TropicalSemiring():
+    def handle_initialisation(self, not_none, n1, n2):
+        if not_none is not None:
+            assert isinstance(not_none, numbers.Number), f"must be a number, is of type {type(not_none)}"
+            self.value = np.max(0, not_none)
+            if n1 is not None or n2 is not None:
+                raise MultipleInitialisation
+                    
+    def handle_relevance(self, relevance, n1, n2):
+        self.handle_initialisation(relevance, n1, n2)
+
+    def handle_activation(self, activation, n1, n2):
+        self.handle_initialisation(activation, n1, n2)
+    
+    def handle_weight(self, weight, n1, n2):
+        self.handle_initialisation(weight, n1, n2)
+
+    def __init__(self, relevance=None, activation=None, weight=None):
+        self.handle_relevance(relevance, activation, weight)
+        self.handle_activation(activation, relevance, weight)
+        self.handle_weight(weight, activation, relevance)
+
+    def __mul__(self, b):
+        f = self.value + b.value
+        s = TropicalSemiring()
+        s.value = f
+        return s
+    
+    def to_float(self):
+        return self.value
+    
+    def __add__(self, b):
+        f = np.min(self.value, b.value)
+        s = TropicalSemiring()
+        s.value = f
+        return s
+    
+    from_activation = np.vectorize(lambda x : Semiring(activation=x))
+    from_weight = np.vectorize(lambda x : Semiring(weight=x))
+    vect_pos = np.vectorize(lambda x : x.pos())
+    vect_neg = np.vectorize(lambda x : x.neg())
+
+    def from_relevance(m):
+        return np.array([[Semiring(activation=m[x][y]) for y in range(m.shape[1])] for x in range(m.shape[0])])
+
+    def semiring_to_float(m):
+        return np.array([[m[x][y].to_float() for y in range(m.shape[1])] for x in range(m.shape[0])])
+    
+    def __repr__(self):
+        return f"TropicalSemiring({self.value})"
