8ca451d5f42bc32e2c08cad53c377959e1cc76e3
Red Rapious <red.rapious@gmail.com>
2024-06-10 15:32:12 +0200

    Added counting semiring

--- code/ProvLRP.py
+++ code/ProvLRP.py
@@ -89,7 +89,7 @@ class BooleanSemiring():
         if value is None:
             return None
         return value >= theta
-    
+
     def handle_initialisation(self, not_none, n1, n2):
         if not_none is not None:
             # TODO: test somewhere else in the pipeline that
@@ -141,8 +141,6 @@ class BooleanSemiring():
     
     from_activation = np.vectorize(lambda x : BooleanSemiring(activation=x))
     from_weight = np.vectorize(lambda x : BooleanSemiring(weight=x))
-    vect_pos = np.vectorize(lambda x : x.pos())
-    vect_neg = np.vectorize(lambda x : x.neg())
 
     def from_relevance(m):
         return np.array([[BooleanSemiring(activation=m[x][y]) for y in range(m.shape[1])] for x in range(m.shape[0])])
@@ -191,16 +189,82 @@ class TropicalSemiring():
         s.value = f
         return s
     
-    from_activation = np.vectorize(lambda x : Semiring(activation=x))
-    from_weight = np.vectorize(lambda x : Semiring(weight=x))
-    vect_pos = np.vectorize(lambda x : x.pos())
-    vect_neg = np.vectorize(lambda x : x.neg())
+    from_activation = np.vectorize(lambda x : TropicalSemiring(activation=x))
+    from_weight = np.vectorize(lambda x : TropicalSemiring(weight=x))
 
     def from_relevance(m):
-        return np.array([[Semiring(activation=m[x][y]) for y in range(m.shape[1])] for x in range(m.shape[0])])
+        return np.array([[TropicalSemiring(activation=m[x][y]) for y in range(m.shape[1])] for x in range(m.shape[0])])
 
     def semiring_to_float(m):
         return np.array([[m[x][y].to_float() for y in range(m.shape[1])] for x in range(m.shape[0])])
     
     def __repr__(self):
         return f"TropicalSemiring({self.value})"
+    
+
+class CountingSemiring():
+    def threshold(value, theta):
+        if value is None:
+            return None
+        return 1 if value >= theta else 0
+    
+    def handle_initialisation(self, not_none, n1, n2):
+        if not_none is not None:
+            assert isinstance(not_none, numbers.Number), f"must be a number, is of type {type(not_none)}"
+            self.value = not_none
+            if n1 is not None or n2 is not None:
+                raise MultipleInitialisation
+                    
+    def handle_relevance(self, relevance, n1, n2):
+        relevance_i = CountingSemiring.threshold(relevance, ProvLRP_config.counting_relevance_threshold)
+        #relevance_i = max(0, int(relevance))
+        self.handle_initialisation(relevance_i, n1, n2)
+
+    def handle_activation(self, activation, n1, n2):
+        activation_i = CountingSemiring.threshold(activation, ProvLRP_config.counting_activation_threshold)
+        #activation_i = max(0, int(activation))
+        self.handle_initialisation(activation_i, n1, n2)
+    
+    def handle_weight(self, weight, n1, n2):
+        weight_i = CountingSemiring.threshold(weight, ProvLRP_config.counting_weight_threshold)
+        #weight_i = max(0, int(weight))
+        self.handle_initialisation(weight_i, n1, n2)
+
+    def __init__(self, relevance=None, activation=None, weight=None):
+        self.handle_relevance(relevance, activation, weight)
+        self.handle_activation(activation, relevance, weight)
+        self.handle_weight(weight, activation, relevance)
+
+    def __mul__(self, b):
+        f = self.value * b.value
+        s = CountingSemiring()
+        s.value = f
+        return s
+    
+    def to_float(self):
+        return self.value
+    
+    def __add__(self, b):
+        f = self.value + b.value
+        s = CountingSemiring()
+        s.value = f
+        return s
+    
+    def __sub__(self, b):
+        print("Warning, should not be used without zB rule")
+        f = max(0, self.value - b.value)
+        s = CountingSemiring()
+        s.value = f
+        return s
+    
+    from_activation = np.vectorize(lambda x : CountingSemiring(activation=x))
+    from_weight = np.vectorize(lambda x : CountingSemiring(weight=x))
+
+    def from_relevance(m):
+        return np.array([[CountingSemiring(activation=m[x][y]) for y in range(m.shape[1])] for x in range(m.shape[0])])
+
+    def semiring_to_float(m):
+        return np.array([[m[x][y].to_float() for y in range(m.shape[1])] for x in range(m.shape[0])])
+    
+    def __repr__(self):
+        return f"CountingSemiring({self.value})"
--- code/ProvLRP_config.py
+++ code/ProvLRP_config.py
@@ -1,3 +1,7 @@
 boolean_relevance_threshold = 0.5
 boolean_activation_threshold = 1
 boolean_weight_threshold = 0
+
+counting_relevance_threshold = 0.5
+counting_activation_threshold = 0
+counting_weight_threshold = 0
