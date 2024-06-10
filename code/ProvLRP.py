ff8ab0c175f4ceb91c9b5a1fbc2d22d04d29c6bc
Red Rapious <red.rapious@gmail.com>
2024-06-10 11:18:56 +0200

    Implemented boolean semiring

--- code/ProvLRP.py
+++ code/ProvLRP.py
@@ -78,3 +78,88 @@ class Semiring():
 
     def semiring_to_float(m):
         return np.array([[m[x][y].to_float() for y in range(m.shape[1])] for x in range(m.shape[0])])
+    
+    def __repr__(self):
+        return f"Semiring({self.value})"
+    
+
+class BooleanSemiring():
+    def threshold(value, theta):
+        if value is None:
+            return None
+        return value >= theta
+    
+    def handle_initialisation(self, not_none, n1, n2):
+        if not_none is not None:
+            # TODO: test somewhere else in the pipeline that
+            # the parameter is correct (below, the parameter)
+            # is already transformed in semiring element, so it's too late
+            #assert isinstance(not_none, numbers.Number), f"must be a number, is of type {type(not_none)}"
+            self.value = not_none
+            if n1 is not None or n2 is not None:
+                raise MultipleInitialisation
+                    
+    def handle_relevance(self, relevance, n1, n2):
+        relevance_b = BooleanSemiring.threshold(relevance, 0)
+        self.handle_initialisation(relevance_b, n1, n2)
+
+    def handle_activation(self, activation, n1, n2):
+        activation_b = BooleanSemiring.threshold(activation, 0.5)
+        self.handle_initialisation(activation_b, n1, n2)
+    
+    def handle_weight(self, weight, n1, n2):
+        weight_b = BooleanSemiring.threshold(weight, 0)
+        self.handle_initialisation(weight_b, n1, n2)
+
+    def __init__(self, relevance=None, activation=None, weight=None):
+        self.handle_relevance(relevance, activation, weight)
+        self.handle_activation(activation, relevance, weight)
+        self.handle_weight(weight, activation, relevance)
+
+    def __mul__(self, b):
+        f = self.value and b.value
+        s = BooleanSemiring()
+        s.value = f
+        return s
+    
+    def to_float(self):
+        return 1 if self.value else 0
+    
+    def __add__(self, b):
+        f = self.value or b.value
+        s = BooleanSemiring()
+        s.value = f
+        return s
+    
+    def __sub__(self, b):
+        print("Warning, should not be used without zB rule")
+        f = self.value and not b.value
+        s = BooleanSemiring()
+        s.value = f
+        return s
+    
+    def pos(self):
+        assert False, "not defined for boolean semiring"
+
+    def neg(self):
+        assert False, "not defined for boolean semiring"
+    
+    def lower_bound(shape, _bound):
+        assert False, "not defined for boolean semiring"
+
+    def upper_bound(shape, _bound):
+        assert False, "not defined for boolean semiring"
+    
+    from_activation = np.vectorize(lambda x : BooleanSemiring(activation=x))
+    from_weight = np.vectorize(lambda x : BooleanSemiring(weight=x))
+    vect_pos = np.vectorize(lambda x : x.pos())
+    vect_neg = np.vectorize(lambda x : x.neg())
+
+    def from_relevance(m):
+        return np.array([[BooleanSemiring(activation=m[x][y]) for y in range(m.shape[1])] for x in range(m.shape[0])])
+
+    def semiring_to_float(m):
+        return np.array([[m[x][y].to_float() for y in range(m.shape[1])] for x in range(m.shape[0])])
+    
+    def __repr__(self):
+        return f"BooleanSemiring({self.value})"
